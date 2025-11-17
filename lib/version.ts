43b713260d8dc9e027583ef61c625800d53b787f
LMS Platform Deploy <deploy@lms-platform.local>
2025-11-17 13:41:39 +0300

    chore: refresh version badge info

--- lib/version.ts
+++ lib/version.ts
@@ -2,6 +2,6 @@
 // Автоматически обновляется при изменении package.json и сборке
 
 export const APP_VERSION: string = '1.0.0';
-export const COMMIT_HASH: string = 'd95a119';
-export const COMMIT_DATE: string = '2025-11-16 21:50:30';
-export const GIT_BRANCH: string = 'production';
+export const COMMIT_HASH: string = 'efdb71b';
+export const COMMIT_DATE: string = '2025-11-17 13:39:23';
+export const GIT_BRANCH: string = 'stage';
